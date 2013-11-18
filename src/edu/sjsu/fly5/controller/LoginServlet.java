d961dffadeb30b358c0644626480f5ca7ac80665
Vijaya <Vijaya@10.185.199.128>
2013-11-17 18:52:28 -0800

    commit

--- src/edu/sjsu/fly5/controller/LoginServlet.java
+++ src/edu/sjsu/fly5/controller/LoginServlet.java
@@ -1,10 +1,12 @@
 package edu.sjsu.fly5.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
@@ -25,6 +27,7 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
 	}
 
 	/**
@@ -32,6 +35,7 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
 	}
 
 }
