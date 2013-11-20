31375281f24b2bcb4759bd744b90a319f341f8d4
Vijaya <Vijaya@10.0.0.20>
2013-11-20 12:05:38 -0800

    updated messages

--- src/edu/sjsu/fly5/pojos/Attribute.java
+++ src/edu/sjsu/fly5/pojos/Attribute.java
@@ -4,43 +4,31 @@ public class Attribute
 {
 	private String attributeName;
 	private String attributeValue;
-	
-	
-	
-	
+
 	public Attribute() {
 		super();
 	}
 
-
 	public Attribute(String attributeName, String attributeValue) {
-	
+
 		this.attributeName = attributeName;
 		this.attributeValue = attributeValue;
 	}
-
 
 	public String getAttributeName() {
 		return attributeName;
 	}
 
-
 	public void setAttributeName(String attributeName) {
 		this.attributeName = attributeName;
 	}
-
 
 	public String getAttributeValue() {
 		return attributeValue;
 	}
 
-
 	public void setAttributeValue(String attributeValue) {
 		this.attributeValue = attributeValue;
 	}
-	
-	
-	
-	
-	
+
 }
